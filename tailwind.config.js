29155299a6bb162d411d858ee6acfa32906897ed
ChloeLee01 <youngsunyi01@gmail.com>
2024-11-06 16:32:14 +0900

    initial setting

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+rts = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      customFontEn: ["IBM Plex Mono", "monospace"],
+      customFontEn: ["IBM Plex Sans KR", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
