541acb2872ae63a8d299fa6c2d4a550c34e10769
ChloeLee01 <youngsunyi01@gmail.com>
2024-11-11 14:31:59 +0900

    post

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -1,6 +1,14 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { DELETE_TASK_API_URL, GET_TASKS_API_URL } from "../../utils/apiUrl";
-import { deleteRequest, getRequest } from "../../utils/requestMethods";
+import {
+  DELETE_TASK_API_URL,
+  GET_TASKS_API_URL,
+  POST_TASK_API_URL,
+} from "../../utils/apiUrl";
+import {
+  deleteRequest,
+  getRequest,
+  postRequest,
+} from "../../utils/requestMethods";
 
 const getItemsFetchThunk = (actionType, apiURL) => {
   return createAsyncThunk(actionType, async (userId) => {
@@ -34,6 +42,23 @@ export const fetchDeleteItemData = deleteItemFetchThunk(
   DELETE_TASK_API_URL
 );
 
+// post thunk function 정의
+const postItemFetchThunk = (actionType, apiURL) => {
+  return createAsyncThunk(actionType, async (postData) => {
+    // console.log(postData);
+    const options = {
+      body: JSON.stringify(postData), // 표준 json 문자열로 변환
+    };
+    return await postRequest(apiURL, options);
+  });
+};
+
+// post_item
+export const fetchPostItemData = postItemFetchThunk(
+  "fetchPostItem",
+  POST_TASK_API_URL
+);
+
 // handleFulfilled 함수 정의 : 요청 성공시 상태 업데이트 로직을 별도의 함수로 정의
 const handleFulfilled = (stateKey) => (state, action) => {
   state[stateKey] = action.payload; // action.payload에 응답 데이터가 들어있음
@@ -52,14 +77,18 @@ const apiSlice = createSlice({
     // 초기 상태 지정
     getItemsData: null,
     deleteItemData: null,
+    postItemData: null,
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchGetItemsData.fulfilled, handleFulfilled("getItemsData")) //요청 성공시
       .addCase(fetchGetItemsData.rejected, handleRejected) // 요청 실패시
-      .addCase(fetchDeleteItemData.fulfilled, handleFulfilled("deleteItemData")) 
-      .addCase(fetchDeleteItemData.rejected, handleRejected);
 
+      .addCase(fetchDeleteItemData.fulfilled, handleFulfilled("deleteItemData"))
+      .addCase(fetchDeleteItemData.rejected, handleRejected)
+
+      .addCase(fetchPostItemData.fulfilled, handleFulfilled("postItemData"))
+      .addCase(fetchPostItemData.rejected, handleRejected);
   },
 }); // slice 객체 저장
 
